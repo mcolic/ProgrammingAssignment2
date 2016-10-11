d919eb47a75daa77ff88571d4cd6a43819c8441c
mcolic <medina.colic@gmail.com>
2016-10-11 11:13:01 -0500

    updates

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
 
-  GET <- function() x
+  get <- function() x
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
@@ -33,7 +33,7 @@ cacheSolve <- function(x, ...) {
     return(inv)
   }
   data <- x$get()
-  inv <- solve(data)
+  inv <- solve(data, ...)
   x$setinverse(inv)
   inv
 }
